d28c92587a357877ba8a121aeae769afd5fb7eb0
Carl Zulauf <carl@linkleaf.com>
2022-08-28 17:26:26 -0600

    Refactor remaining proxy trace functionality into tracer class
    This allows complete elimination of all method definitions within the proxy, other than the methods we are tracing.

--- lib/markdown_logging_proxy/proxy.rb
+++ lib/markdown_logging_proxy/proxy.rb
@@ -17,85 +17,29 @@ module MarkdownLoggingProxy
         proxy_response: [], # methods we should return a proxy for
         overwrite: [] # methods defined on Object we should overwrite
       )
-      @ignore = (ignore + proxy_response).uniq
-      @proxy_response = proxy_response
       @target = to_proxy || target
       raise ArgumentError, "Missing required proxy target" unless @target
-      @logger = MarkdownLogger.new(location, backtrace: backtrace)
+      @logger = MarkdownLogger.build(location, backtrace: backtrace)
+      @tracer = Tracer.new(
+        target: @target,
+        proxy: self,
+        logger: @logger,
+        ignore: ignore,
+        proxy_response: proxy_response,
+        proxy_options: {
+          overwrite: overwrite,
+          backtrace: backtrace,
+        }
+      )
       overwrite.each do |meth|
         define_method(meth) do |*args, &blk|
-          __trace_method(meth, args, &blk)
+          @traceer.trace(meth, args, &blk)
         end
       end
     end
 
     def method_missing(meth, *args, &blk)
-      __trace_method(meth, args, &blk)
-    end
-
-    private
-
-    def __trace_method(meth, args, &blk)
-      @logger.log :info, 1, <<~MSG
-        Calling `#{meth}`
-
-        Arguments:
-
-        #{MarkdownLogger.inspect_object(args)}
-
-        Block? #{block_given? ? 'Yes' : 'No'}
-        #{@logger.inspect_backtrace}
-      MSG
-
-      __proxy_response(meth, @target.public_send(meth, *args, &__proxy_block(meth, blk))).tap do |response|
-        @logger.log :info, 2, <<~MSG
-          `#{meth}` Response
-
-          #{MarkdownLogger.inspect_object(response)}
-        MSG
-      end
-    rescue StandardError => e
-      @logger.log :error, 2, <<~MSG
-        Error in `#{meth}`
-
-        Type: #{e.class}
-
-        #{MarkdownLogger.inspect_object(e)}
-      MSG
-      raise e
-    end
-
-    def __proxy_response(meth, response)
-      return response unless @proxy_response.member?(meth)
-      @logger.log :info, 3, <<~MSG
-        Using proxied response for `#{meth}`
-
-        Object to proxy:
-
-        #{MarkdownLogger.inspect_object(response)}
-      MSG
-      self.class.new(target: response, logger: @logger)
-    end
-
-    def __proxy_block(meth, block)
-      return if block.nil?
-      logger = @logger
-      proc do |*args|
-        logger.log :info, 3, <<~MSG
-          Yield to block in `#{meth}`
-
-          Arguments:
-
-          #{MarkdownLogger.inspect_object(args)}
-        MSG
-        block.call(*args).tap do |response|
-          logger.log :info, 4, <<~MSG
-            Response from block in `#{meth}`
-
-            #{MarkdownLogger.inspect_object(response)}
-          MSG
-        end
-      end
+      @tracer.trace(meth, args, &blk)
     end
   end
 end
---
+++ lib/markdown_logging_proxy/tracer.rb
@@ -0,0 +1,114 @@
+module MarkdownLoggingProxy
+  class Tracer
+    attr_reader :target, :logger, :ignore, :proxy
+    
+    def initialize(
+        target:,
+        proxy:,
+        logger: nil,
+        ignore: [],
+        proxy_response: [],
+        proxy_options: {}
+      )
+      @target = target
+      @logger = logger
+      @ignore = (ignore + proxy_response)
+      @proxy_response = proxy_response
+      @proxy_options = proxy_options
+    end
+
+    def trace(meth, args, &blk)
+      log_call_signature(meth, args, &blk) unless ignore?(meth)
+      log_and_proxy_response(meth, args, &blk)
+    rescue StandardError => e
+      log_and_reraise_error(meth, e)
+    end
+
+    def proxy_response?(meth)
+      case @proxy_response
+      when true, false then @proxy_response
+      else
+        @proxy_response.member?(meth)
+      end
+    end
+
+    def ignore?(meth)
+      @ignore.member?(meth)
+    end
+
+    private
+
+    def log_call_signature(meth, args, &blk)
+      return if ignore.member?(meth)
+      logger.log :info, 1, <<~MSG.chomp
+        Calling `#{meth}` on #{MarkdownLogger.id_object(target)}
+
+        Arguments:
+
+        #{MarkdownLogger.inspect_object(args)}
+
+        Block? #{block_given? ? 'Yes' : 'No'}
+        #{logger.inspect_backtrace}
+      MSG
+    end
+
+    def log_and_proxy_response(meth, args, &blk)
+      response = target.public_send(meth, *args, &log_and_proxy_block(meth, blk))
+      log_response(meth, response) unless ignore?(meth)
+      return response unless proxy_response?(meth)
+      logger.log :info, 3, <<~MSG
+        Returning proxied response to `#{meth}`
+        
+        Proxy from `#{meth}` on #{MarkdownLogger.id_object(target)}
+        
+        Proxy for: #{MarkdownLogger.id_object(response)}
+      MSG
+      Proxy.new(**@proxy_options.merge(
+        target: response,
+        location: logger,
+        proxy_response: @proxy_response,
+        ignore: @ignore,
+      ))
+    end
+
+    def log_and_proxy_block(meth, blk)
+      return if blk.nil?
+      proc do |*args|
+        logger.log :info, 3, <<~MSG
+          Yield to block in `#{meth}` on #{MarkdownLogger.id_object(target)}
+
+          Arguments:
+
+          #{MarkdownLogger.inspect_object(args)}
+        MSG
+        blk.call(*args).tap do |response|
+          logger.log :info, 4, <<~MSG
+            Response from block in `#{meth}`
+
+            #{MarkdownLogger.inspect_object(response)}
+          MSG
+        end
+      end
+    end
+
+    def log_and_reraise_error(meth, error)
+      logger.log :error, 2, <<~MSG
+        Error in `#{meth}`
+
+        Type: #{error.class}
+
+        #{MarkdownLogger.inspect_object(error)}
+      MSG
+      raise error
+    end
+
+    def log_response(meth, response)
+      return if ignore?(meth)
+      logger.log :info, 2, <<~MSG.chomp
+        `#{meth}` response
+
+        #{MarkdownLogger.inspect_object(response)}
+      MSG
+    end
+  end
+end
